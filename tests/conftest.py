a7e46ea312fde0c269683a69b380655c6fd611a4
Zane-Emery <zwe2@cornell.edu>
2026-04-27 18:34:10 -0700

    Add uv.lock and drop conftest sys.path hack

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, project_root)
-
-src_path = os.path.join(project_root, 'src')
-sys.path.insert(0, src_path)
